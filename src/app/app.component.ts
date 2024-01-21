537f5ffa117909cd6e72b43c509d7eb50a7020f9
AmmarJS <ammar552259@gmail.com>
2024-01-21 19:40:29 +0300

    added basic navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  loadedFeature: string = 'recipe';
+
+  renderFeature(feature: string){
+    this.loadedFeature = feature;
+  }
 }

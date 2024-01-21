91bdca4f902d3133cc9d55e60ea54fd8dbd50a80
AmmarJS <ammar552259@gmail.com>
2024-01-21 20:37:35 +0300

    added a white details screen if no recipe is selected

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -7,7 +7,7 @@ import { Recipe } from './recipe.model';
   styleUrls: ['./recipes.component.css']
 })
 export class RecipesComponent {
-  recipe: Recipe = new Recipe('test', 'test', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTROsR7YTv67gUzkQ3W1S6Y6N0REbyC7I8rIQ&usqp=CAU');
+  recipe: Recipe | null = null;
 
   showRecipe(recipe: Recipe) {
     this.recipe = recipe;
